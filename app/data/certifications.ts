46e01a73f8d58fe81306b4d566370c992a804573
Ed <eddiewwu@gmail.com>
2026-08-10 11:31:03 -0500

    fix: Update Cert URLs

--- app/data/certifications.ts
+++ app/data/certifications.ts
@@ -10,13 +10,13 @@ export const certifications: Certification[] = [
     title: "AWS Certified Developer – Associate",
     issuer: "Amazon Web Services",
     date: "Apr 2024",
-    link: "https://cp.certmetrics.com/amazon/en/public/verify/credential/469e04bc0dd244c0bc543cc9d2780f8e",
+    link: "https://www.credly.com/badges/de8a3671-dbab-430e-86d6-e4d95f164568",
   },
   {
     title: "AWS Certified Solutions Architect – Associate",
     issuer: "Amazon Web Services",
     date: "Sept 2024",
-    link: "https://cp.certmetrics.com/amazon/en/public/verify/credential/0d950334eca3480786a8f5c40f3b1df7",
+    link: "https://www.credly.com/badges/77fe8174-82a7-45d5-b83f-03294fe7f91d",
   },
   {
     title: "AWS Certified Solutions Architect – Professional",
